521b50d74551c168afb00213646c71e29cb3994d
Leo Koo <leokoo@gmail.com>
2016-01-07 19:53:42 +0800

    3 errors for model, 2 for controller

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,8 @@
 class Url < ActiveRecord::Base
 	validates :long_url, 
-		presence: true
+		presence: true,
+		:format => URI::regexp(%w(http https))
+
 	validates :short_url,
 		presence: true
 end
