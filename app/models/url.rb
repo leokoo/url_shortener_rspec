6487dd9916f4b87bec4f80f55b56043ddf965212
Leo Koo <leokoo@gmail.com>
2016-01-08 15:43:31 +0800

    fixed error with model

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,9 +6,4 @@ class Url < ActiveRecord::Base
 	validates :short_url,
 		presence: true
 
-	def self.reterieve_short_url(long_url)
-		Url.find_by long_url(long_url)
-			return self.short_url unless self.nil?
-		nil
-	end
 end
