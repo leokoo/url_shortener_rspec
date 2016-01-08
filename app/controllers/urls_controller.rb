3c873a1be8ac5e3cdbfbb907b8fd570068ae78e4
Leo Koo <leokoo@gmail.com>
2016-01-08 16:29:41 +0800

    all solved

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,30 +3,30 @@ class UrlsController < ActionController::Base
     @urls = Url.all
   end
 
-  def create
-    @url = Url.new(long_url: params[:long_url])
-    @errors = "error"
-    if !params[:long_url].nil?
-      @url.short_url = SecureRandom.base64[0..8]
-      @url.save
-      redirect_to urls_path
-    else
-      render :new
-    end
-  end
-
   # def create
-  #   @url = Url.new(url_params)
-  #   if @url.save
+  #   @url = Url.new(long_url: params[:long_url])
+  #   @errors = "error"
+  #   if !params[:long_url].nil?
+  #     @url.short_url = SecureRandom.base64[0..8]
+  #     @url.save
+  #     redirect_to urls_path
   #   else
-  #     @errors = "errors here"
+  #     render :new
   #   end
-  #   redirect_to urls_path
   # end
+
+  def create
+    @url = Url.new(url_params)
+    if @url.save
+    else
+      @errors = "errors here"
+    end
+    redirect_to urls_path
+  end
 
   private
     def url_params
-      params.permit[:long_url].merge(short_url: SecureRandom.base64[0..8])
+      params.permit(:long_url).merge(short_url: SecureRandom.base64[0..8])
     # params[:long_url]
     end
 end
