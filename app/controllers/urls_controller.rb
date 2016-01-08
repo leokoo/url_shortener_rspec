930e577b792f9e24ea8d7604a9ec50b26c8be99a
Leo Koo <leokoo@gmail.com>
2016-01-08 16:05:21 +0800

    solved all errors

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,16 +4,29 @@ class UrlsController < ActionController::Base
   end
 
   def create
-    @url = Url.new(url_params)
-    if @url.save
+    @url = Url.new(long_url: params[:long_url])
+    @errors = "error"
+    if !params[:long_url].nil?
+      @url.short_url = SecureRandom.base64[0..8]
+      @url.save
       redirect_to urls_path
     else
-      @errors = "errors here"
+      render :new
     end
   end
 
+  # def create
+  #   @url = Url.new(url_params)
+  #   if @url.save
+  #   else
+  #     @errors = "errors here"
+  #   end
+  #   redirect_to urls_path
+  # end
+
   private
     def url_params
-      params.permit[:long_url]
+      params.permit[:long_url].merge(short_url: SecureRandom.base64[0..8])
+    # params[:long_url]
     end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -6,4 +6,10 @@ class Url < ActiveRecord::Base
 	validates :short_url,
 		presence: true
 
+	def self.reterive_short_url(long_url)
+		url = Url.find_by(long_url: long_url)
+		if url
+			url.short_url
+		end
+	end
 end
